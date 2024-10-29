c08caeebf05a3feef77c0939ae02cb40a6962aac
zahra <zahraaeemami@gmail.com>
2024-10-29 18:00:43 +0330

    add lang fa-ir

--- library/settings.py
+++ library/settings.py
@@ -103,13 +103,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir' # 'en-us'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = True
-
-USE_TZ = True
+USE_I18N = True  
+USE_L10N = True  
+USE_TZ = True  
 
 
 # Static files (CSS, JavaScript, Images)
